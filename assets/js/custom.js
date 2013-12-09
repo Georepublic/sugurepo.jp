a54c5c7ac1776f9aed601a89667b940bdbb88a96
Daniel Kastl <daniel@georepublic.de>
2013-12-09 15:32:40 +0900

    newsletter unsubscribe

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,12 +18,35 @@ jQuery(document).ready(function() {
 							alert("Invalid email address.");
 							break;
 
-						case 'Invalid list ID.':
-							alert("Invalid list ID.");
+						default:
+							alert("You're subscribed!");
+							break;
+					}
+				}
+				else {
+					alert("Sorry, unable to subscribe. Please try again later!");
+				}			
+			}
+		);
+	});
+
+	$('button.unsubscribe').bind('click', function (evt) {
+		$.post(
+			$(this.form)[0].action, 
+			$(this.form).serialize(), 
+			function (response) {
+				if (true) {
+					switch (response) {
+						case 'Some fields are missing.':
+							alert("Some fields are missing.");
+							break;
+
+						case 'Invalid email address.':
+							alert("Invalid email address.");
 							break;
 
 						default:
-							alert("You're subscribed!");
+							alert("You're unsubscribed!");
 							break;
 					}
 				}
